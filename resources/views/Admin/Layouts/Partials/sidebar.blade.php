fcece2cec458e4e6df97bf8d4e0e701d35c1b821
ahmed <a.awad@eightytech.com>
2022-03-05 06:47:08 +0200

    changed siderbar layout for bikers and senders

--- resources/views/Admin/Layouts/Partials/sidebar.blade.php
+++ resources/views/Admin/Layouts/Partials/sidebar.blade.php
@@ -4,13 +4,13 @@
             <i class="ft-home"></i><span class="menu-title">Dashboard</span>
         </a>
     </li>
+    @if(\App\Models\User::isSender(\Illuminate\Support\Facades\Auth::user()))
+        <li class="nav-item"><a href="javascripts::void(0);">
+                <i class="ft-target"></i><span
+                    class="menu-title">Parcels</span>
+            </a>
+            <ul class="menu-content">
 
-    <li class="nav-item"><a href="javascripts::void(0);">
-            <i class="ft-target"></i><span
-                class="menu-title">Parcels</span>
-        </a>
-        <ul class="menu-content">
-            @if(\App\Models\User::isSender(\Illuminate\Support\Facades\Auth::user()))
                 <li class="">
                     <a href="{{route('sender.parcels.index')}}"
                        class="menu-item">Index</a>
@@ -19,14 +19,15 @@
                     <a href="{{route('sender.parcels.create')}}"
                        class="menu-item">Create</a>
                 </li>
-            @elseif(\App\Models\User::isBiker(\Illuminate\Support\Facades\Auth::user()))
-                <li class="">
-                    <a href="{{route('biker.parcels.index')}}"
-                       class="menu-item">Index</a>
-                </li>
-            @endif
-        </ul>
-    </li>
+            </ul>
+        </li>
+
+    @elseif(\App\Models\User::isBiker(\Illuminate\Support\Facades\Auth::user()))
+        <li class="nav-item"><a href="{{route('biker.parcels.index')}}">
+                <i class="ft-target"></i><span class="menu-title">Parcels</span>
+            </a>
+        </li>
+    @endif
 
     @if(\App\Models\User::isBiker(\Illuminate\Support\Facades\Auth::user()))
         <li class="nav-item"><a href="{{route('biker.orders.index')}}">
